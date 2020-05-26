c5609d68aa1862b0231c49c4f09d2c793240fe89
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-26 14:32:38 +0100

    Adding the create method

--- app/controllers/packets_controller.rb
+++ app/controllers/packets_controller.rb
@@ -2,5 +2,18 @@ class PacketsController < ApplicationController
   def new
     @packet = Packet.new
   end
+
+  def create
+    @packet = Packet.new(packets_params)
+    @packet.user = current_user
+    if @packet.save
+
+  end
+  
+  private
+
+  def packets_params
+    params.require    
+  end
   
 end
