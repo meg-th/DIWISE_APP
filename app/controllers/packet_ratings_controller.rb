27b46a334c9e620cc1566041ac631d5c7405d1fc
Akane Hirabayashi <akanehirabayashi@gmail.com>
2020-05-26 18:03:11 +0100

    fixed model and updated controller

--- app/controllers/packet_ratings_controller.rb
+++ app/controllers/packet_ratings_controller.rb
@@ -1,2 +1,19 @@
 class PacketRatingsController < ApplicationController
+
+  def create
+    @packet = Packet.find(paramas[:packet_id])
+    @rating = Rating.new(rating_params)
+    @rating.user = current_user
+    @rating.packet = @packet
+    if @rating.save
+      rediect_to packet_path(@packet)
+    else
+      render "packet/show"
+    end
+  end
+
+  private
+  def rating_params
+    params.require(:rating).permit(:rating, :packet_i)
+  end
 end
--- app/models/packet_rating.rb
+++ app/models/packet_rating.rb
@@ -1,6 +1,4 @@
 class PacketRating < ApplicationRecord
   belongs_to :packet
   belongs_to :user
-
-  validates :rating, presence: true, numericality: {only_integer: true}, inclusion: { in: [*0..10] }
 end
