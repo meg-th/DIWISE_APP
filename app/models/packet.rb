1b396841ead0118a779661b1803bc2f10c1e6e44
Megan Thresh <meganthresh96@gmail.com>
2020-05-27 16:24:46 +0100

    navbar progress and search bar implementing

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -7,4 +7,10 @@ class Packet < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   has_one_attached :media
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_category,
+      against: [ :title, :category ],
+      using: {tsearch: { prefix: true }
 end
+
