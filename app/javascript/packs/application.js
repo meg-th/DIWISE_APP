d67274212679a4b9ac03e85405c406b9dba0d61d
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-27 17:52:43 +0100

    Implemented the upvote function on the packet card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,11 +18,15 @@ require("channels")
 // ----------------------------------------------------
 // External imports
 import "bootstrap";
+import "controllers";
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
 
 
