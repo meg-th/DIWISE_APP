c1b46c03c5bbf726a1291dbce6471bc78ed10082
Arthur Littmann <arthur@lewagon.org>
2020-05-28 11:59:33 +0100

    removed stimulus, doing in rails and turbolinks

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -8,14 +8,19 @@ class Packet < ApplicationRecord
   has_many_attached :photos
   has_one_attached :video
   before_save :render_url
-  
+  has_many :packet_ratings
+
   include PgSearch::Model
   pg_search_scope :search_by_title_and_category,
       against: [ :title, :category ],
       using: {tsearch: { prefix: true }}
-  
+
   def render_url
     youtube_url.gsub!('https://www.youtube.com/watch?v=', '')
+  end
+
+  def rating
+    packet_ratings.count
   end
 
 end
