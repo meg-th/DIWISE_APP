1e57ea49b4539f000cae2633f54eb8612835d58d
Megan Thresh <meganthresh96@gmail.com>
2020-06-02 10:11:50 +0100

    progress

--- app/controllers/packet_tools_controller.rb
+++ app/controllers/packet_tools_controller.rb
@@ -3,7 +3,7 @@ class PacketToolsController < ApplicationController
     @packet = Packet.find(params[:packet_id])
     @packet_tool = PacketTool.new
     @packet_tool.packet = @packet
-    @packet_tool.tool = Tool.find(params["packet_tool"]["tool"])
+    @packet_tool.tool = Tool.find(params[:packet_tool][:tool])
     if @packet_tool.save
       redirect_to packets_path(anchor: "packet-#{@packet.id}")
     else
