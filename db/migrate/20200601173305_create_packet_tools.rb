1f95d24d4a1924d883569f072edd3ab0ae0ea550
Megan Thresh <meganthresh96@gmail.com>
2020-06-02 12:07:13 +0100

    completed add tools to packet function

--- db/migrate/20200601173305_create_packet_tools.rb
+++ db/migrate/20200601173305_create_packet_tools.rb
@@ -1,8 +1,8 @@
 class CreatePacketTools < ActiveRecord::Migration[6.0]
   def change
     create_table :packet_tools do |t|
-      t.references :packets, null: false, foreign_key: true
-      t.references :tools, null: false, foreign_key: true
+      t.references :packet, null: false, foreign_key: true
+      t.references :tool, null: false, foreign_key: true
 
       t.timestamps
     end
