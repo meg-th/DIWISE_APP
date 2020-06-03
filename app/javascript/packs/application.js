e418856592cde1b08eb1cb9b7e760e20811c5b7c
Pasquale Troise <troisepasquale1990@gmail.com>
2020-06-03 18:25:27 +0100

    Implementing a carousel in the user show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,12 +26,13 @@ import "controllers";
 // import { initSelect2 } from '../components/init_select2';
 import { initSelect2 } from '../plugins/init_select2';
 import { filter_by } from '../components/filter_by';
+import { carousel } from '../components/carousel';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   filter_by();
   initSelect2();
+  carousel();
 });
 
-import "controllers"
