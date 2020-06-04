48ab1675d41c9508a1446dc915fdd8032d3e4b2d
Pasquale Troise <troisepasquale1990@gmail.com>
2020-06-04 19:22:29 +0100

    Implemented a delete butto/feature for the packets card that works only for the right user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :packets, only: [:new, :create, :index, :show] do
+  resources :packets, only: [:new, :create, :index, :show, :destroy] do
     resources :packet_ratings, only: [:create] # upvote
     resources :project_packets, only: [:create]
     resources :packet_tools, only: [:create]
