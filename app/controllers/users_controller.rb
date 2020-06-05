1a7c38f045d4649cbaeebb4d6ff5e331b84ff9bc
Eloise Tait <eloisetait@yahoo.com>
2020-06-05 15:36:34 +0100

    photos load first rather than youtube

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @sample_image = ["paint.jpg", "hanging-bulbs.jpg", "interior.jpg", "interior2.jpg", "interior3.jpg", "interior4.jpg", "garden.jpg", "garden2.jpg"]
+    @sample_image = ["paint.jpg", "hanging-bulbs.jpg", "interior.jpg", "interior2.jpg", "interior3.jpg", "interior4.jpg", "garden.jpg"]
     @chatrooms = Chatroom.joins(:chatroom_users).where(chatroom_users:{user_id: current_user.id})
     @started_chatrooms = @user.chatrooms.select { |chatroom| chatroom.packet.user != @user }
     @packet_chatrooms = @user.chatrooms.select { |chatroom| chatroom.packet.user == @user }
